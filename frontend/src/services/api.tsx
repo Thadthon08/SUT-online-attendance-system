0265a45e6c42001e32aad2fd7064939232b66d55
Thuch_o_o <91382929+Thadthon08@users.noreply.github.com>
2024-09-17 17:36:39 +0700

    deploy

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -4,8 +4,8 @@ import { LoginInterface } from "../interface/ILogin";
 import { LoginResponseInterface } from "../interface/ILoginRespon";
 import { AttendanceRoomResponse } from "../interface/IAttendanceRoomresponse";
 
-// const apiURL = "https://sut-online-attendance-system.onrender.com";
-const apiURL = "http://localhost:8080";
+const apiURL = "https://sut-online-attendance-system.onrender.com";
+// const apiURL = "http://localhost:8080";
 
 function getAuthToken() {
   return sessionStorage.getItem("access_token") || "";
